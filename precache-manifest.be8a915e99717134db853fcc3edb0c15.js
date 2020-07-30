f83f01eb95d50b46de373dfdf3412d1a71468f7d
Theralun <mi.reinsperger@gmail.com>
2020-07-30 23:24:10 +0200

    Updates

---
+++ precache-manifest.be8a915e99717134db853fcc3edb0c15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59de8f9463665c977875b5c105ef845e",
+    "url": "/clock/index.html"
+  },
+  {
+    "revision": "47f60b0c28d09a2e412c",
+    "url": "/clock/static/css/main.d43bfc24.chunk.css"
+  },
+  {
+    "revision": "52bda245b60485e4166f",
+    "url": "/clock/static/js/2.cd6fe424.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clock/static/js/2.cd6fe424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47f60b0c28d09a2e412c",
+    "url": "/clock/static/js/main.7842b6bb.chunk.js"
+  },
+  {
+    "revision": "a1d19904f6dff0d22aec",
+    "url": "/clock/static/js/runtime-main.304c2e05.js"
+  }
+]);
