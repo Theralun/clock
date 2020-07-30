8fa77085cc58022c1538d1add7ecf9f00a5a1f76
Theralun <mi.reinsperger@gmail.com>
2020-07-30 23:02:38 +0200

    Add gh-pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = { light: false };
+
+  componentDidMount = async () => {
+    let res = await fetch(
+      "http://worldtimeapi.org/api/timezone/Europe/Vienna"
+    ).then((res) => {
+      return res.json();
+    });
+
+    // for time;
+    const deg = 6;
+    // 360 / (12 * 5);
+
+    const hr = document.querySelector("#hr");
+    const mn = document.querySelector("#mn");
+    const sc = document.querySelector("#sc");
+
+    let offset = 0;
+    let time = res.unixtime * 1000;
+    let tickspeed = 100;
+
+    setInterval(async () => {
+      offset += tickspeed * 1.01;
+
+      if (offset > 30000) {
+        offset = 0;
+
+        res = await fetch(
+          "http://worldtimeapi.org/api/timezone/Europe/Vienna"
+        ).then((res) => {
+          return res.json();
+        });
+
+        time = res.unixtime * 1000;
+      }
+
+      let day = new Date(time + offset);
+      let hh = day.getHours();
+      let mm = day.getMinutes();
+      let ss = day.getSeconds();
+      let msec = day.getMilliseconds();
+
+      let ssms = ss + msec / 1000;
+      let mmssms = mm + ssms / 60;
+      let hhmmssms = hh + mmssms / 60;
+
+      // VERY IMPORTANT STEP:
+
+      hr.style.transform = `rotateZ(${hhmmssms * 30}deg)`;
+      mn.style.transform = `rotateZ(${mmssms * deg}deg)`;
+      sc.style.transform = `rotateZ(${ssms * deg}deg)`;
+
+      this.setState({ hour: hh, minute: mm, second: ss, msecond: msec });
+    }, tickspeed);
+  };
+
+  toggleLight = () => {
+    this.setState({ light: !this.state.light });
+  };
+
+  pad(num, size) {
+    let s = num + "";
+
+    while (s.length < size) s = "0" + s;
+
+    return s;
+  }
+
+  render() {
+    return (
+      <div id="clock" className={this.state.light ? "light" : "dark"}>
+        <div className="clock">
+          <div className="hour">
+            <div className="hr" id="hr"></div>
+          </div>
+
+          <div className="min">
+            <div className="mn" id="mn"></div>
+          </div>
+
+          <div className="sec">
+            <div className="sc" id="sc"></div>
+          </div>
+        </div>
+        {this.state.msecond !== undefined && (
+          <div className="d-flex justify-content-between digital">
+            <div>{this.state.hour}</div>
+            <div>:</div>
+            <div>{this.state.minute}</div>
+            <div>:</div>
+            <div>{this.state.second}</div>
+            <div>:</div>
+            <div>{this.pad(this.state.msecond, 3)}</div>
+          </div>
+        )}
+        <div className="toggleClass" onClick={this.toggleLight}></div>
+      </div>
+    );
+  }
 }
 
 export default App;
